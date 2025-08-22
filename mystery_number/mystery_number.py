6f364f86706df4538a359dc553b0d57fef63257c
Yusuf Yaşar <yusufyasar1301@gmail.com>
2025-08-22 15:08:27 +0300

    updating the icon and images path

--- mystery_number/mystery_number.py
+++ mystery_number/mystery_number.py
@@ -4,6 +4,7 @@ import random
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import askyesno, showinfo, showwarning
+import os
 
 class App(tk.Tk):
 
@@ -12,7 +13,9 @@ class App(tk.Tk):
 
         # main properties of the app
         self.title('Mystery of the Numbers')
-        self.iconbitmap('./icons/numb.ico')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        numb_path = os.path.join(BASE_DIR, "icons", "numb.ico")
+        self.iconbitmap(numb_path)
         self.iconbitmap()
         self.size(350,300)
         self.main_screen()
@@ -37,7 +40,9 @@ class App(tk.Tk):
         self.special_padd1 = {'padx': 5, 'pady': 2.5}
 
         # image used in the app
-        self.str_image = tk.PhotoImage(file='./images/start.png')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        start_path = os.path.join(BASE_DIR, "images", "start.png")
+        self.str_image = tk.PhotoImage(file=start_path)
 
         # features of main page contents 
         self.main_frame = tk.Frame(self,bg="Lavender")
@@ -122,7 +127,9 @@ class App(tk.Tk):
         self.guess_text.pack(fill=tk.X,expand=True,side=tk.LEFT,padx=10)
 
         # features of the guess button
-        self.guess_image = tk.PhotoImage(file='./images/guess.png')
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        guess_path = os.path.join(BASE_DIR, "images", "guess.png")
+        self.guess_image = tk.PhotoImage(file=guess_path)
         self.guess_btn = tk.Button(self.topleftcenter_frame,image=self.guess_image,bg="SteelBlue",activebackground="SteelBlue",bd=0,cursor="hand2",command=self.guess)
         self.guess_btn.pack(expand=True,side=tk.LEFT)
 
